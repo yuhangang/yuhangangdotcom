698c46a85002197353226a7987258026717894aa
yuhangang <redrainhang@gmail.com>
2020-09-21 00:57:13 +0800

    test image placeholder

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,16 @@ import styles from '../styles/page_styles/home.module.scss';
 
 const MobileMain =()=>{
   return(
-    <div className={styles.mainContainerItems}>
+    <div className={styles.placeholder}>
+<div className={styles.mainContainerItems}>
 
-      <div className={styles.mainContainerTitle}>Journey<br></br>Starts Here</div>
-      <a href='/about' className={styles.homeButton}>about me</a>
-      <a href='/projects' className={styles.homeButton}>my works</a>
-      
-      </div>
+<div className={styles.mainContainerTitle}>Journey<br></br>Starts Here</div>
+<a href='/about' className={styles.homeButton}>about me</a>
+<a href='/projects' className={styles.homeButton}>my works</a>
+
+</div>
+    </div>
+    
   )
 }
 
