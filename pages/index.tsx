9b9190f66187000e74e56029bb4faf291ee84dd5
yuhangang <redrainhang@gmail.com>
2020-11-14 14:53:46 +0800

    remove page transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,7 @@ import styles from '../styles/page_styles/home.module.scss';
 const MobileMain =()=>{
   return(
 
-<div className={styles.mainContainerItems} 
-style={{
-  backgroundImage:"url("+`$require("./images/IMG_6812.jpg")`+")"
-}}>
+<div className={styles.mainContainerItems}>
 
 <div className={styles.mainContainerTitle}>Journey<br></br>Starts Here</div>
 <a href='/about' className={styles.homeButton}>about me</a>
