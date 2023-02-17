29a5c463167ccdfebf26bfc79f79ac79cfb81b03
yuhangang <70849672+yuhangang@users.noreply.github.com>
2023-02-17 19:02:24 +0800

    fix build error, update intros

--- components/aboutContainer/timeLine.tsx
+++ components/aboutContainer/timeLine.tsx
@@ -1,80 +1,88 @@
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-import React from 'react';
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
 import styles from "./timeLine.module.scss";
 import tl from "./tl.module.scss";
 
-
-
-
-
-
-interface TimeLineData{
-  dateTime :string;
-  title : string;
-  detail?:string| JSX.Element;
-  link? : string ; 
-} 
-
-function TimeLineItem(data:TimeLineData):JSX.Element {
-  let detail :JSX.Element =data.link ?   <div className={styles.timeLineLink}><Link href={data.link}><div className={tl.timeLineLinkFont}>Explore</div></Link><br></br></div>:<div></div>;
-
-
-  //let detail :JSX.Element =data.link ?   <div className={tl.timeLineLink}>{data.detail}</div>:<div className={tl.timeLineLink}></div>;
- 
-
-  return(
-    <li className={tl.timeline_item}>
-    <div className={tl.timeline_info}>
-        <div style={{wordBreak: "break-word",overflowWrap:"break-word",whiteSpace:"pre-wrap",textOverflow:"ellipsis",maxWidth:"80vw",fontWeight:"lighter"}}>{data.dateTime}</div>
-    </div>
-    <div className={tl.timeline_marker}></div>
-    <div className={tl.timeline_content}>
-        <h3 className={tl.timeline_title}>{data.title}</h3>
-        <p>{data.detail? data.detail:""}</p>
-    {detail}
-            
-    </div>
-</li>
-  );
+interface TimeLineData {
+  dateTime: string;
+  title: string;
+  detail?: string | JSX.Element;
+  link?: string;
 }
 
- 
-// export default function TimeLine(input:{title:string,items:TimeLineData[]}) {
-export default function TimeLine({title, items}: {title: string, items: TimeLineData[]}) {
+function TimeLineItem(data: TimeLineData): JSX.Element {
+  let detail: JSX.Element = data.link ? (
+    <div className={styles.timeLineLink}>
+      <Link href={data.link}>
+        <div className={tl.timeLineLinkFont}>Explore</div>
+      </Link>
+      <br></br>
+    </div>
+  ) : (
+    <div></div>
+  );
 
-
- 
-
- // const timeLine = <Timeline align="left" className={styles.timeLine}>
-//{timeLinesItems.map(timeLineItem)}</Timeline>;
   return (
-<motion.div initial="pageInitial" animate="pageAnimate" variants={{
-  pageInitial: {
-    opacity: 0
-  },
-  pageAnimate: {
-    opacity: 1
-  },
-  
-}}>
-<div className={styles.work}>
-<h2 style={{maxWidth:"100vw",textOverflow:"clip",color:"#6fe9d5"}}>{title}</h2>
-      
-<ul className={`${tl.timeline}${styles.timeline}`}>
-
-{items ? items.map(TimeLineItem):[]}
-
-
-</ul>
-     
+    <li className={tl.timeline_item}>
+      <div className={tl.timeline_info}>
+        <div
+          style={{
+            wordBreak: "break-word",
+            overflowWrap: "break-word",
+            whiteSpace: "pre-wrap",
+            textOverflow: "ellipsis",
+            maxWidth: "80vw",
+            fontWeight: "lighter",
+          }}
+        >
+          {data.dateTime}
+        </div>
       </div>
-      
-
-    
-     
-</motion.div>
-
+      <div className={tl.timeline_marker}></div>
+      <div className={tl.timeline_content}>
+        <h3 className={tl.timeline_title}>{data.title}</h3>
+        <p>{data.detail ? data.detail : ""}</p>
+        {detail}
+      </div>
+    </li>
   );
 }
 
+// export default function TimeLine(input:{title:string,items:TimeLineData[]}) {
+export default function TimeLine({
+  title,
+  items,
+}: {
+  title: string;
+  items: TimeLineData[];
+}) {
+  // const timeLine = <Timeline align="left" className={styles.timeLine}>
+  //{timeLinesItems.map(timeLineItem)}</Timeline>;
+  return (
+    <motion.div
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <div className={styles.work}>
+        <h2
+          style={{ maxWidth: "100vw", textOverflow: "clip", color: "#6fe9d5" }}
+        >
+          {title}
+        </h2>
+
+        <ul className={`${tl.timeline}${styles.timeline}`}>
+          {items ? items.map(TimeLineItem) : []}
+        </ul>
+      </div>
+    </motion.div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,111 @@
-import Head from 'next/head'
-import React from 'react'
-import AboutContainer from '../components/aboutContainer/aboutContainer'
-import Skills from '../components/aboutContainer/skills'
-import TimeLine from '../components/aboutContainer/timeLine'
-import Layout, { siteTitle } from '../components/layout'
-import ResponsiveComponent from '../components/responsiveComponent/responsive'
-import styles from '../styles/page_styles/about.module.scss'
+import Head from "next/head";
+import React from "react";
+import AboutContainer from "../components/aboutContainer/aboutContainer";
+import Skills from "../components/aboutContainer/skills";
+import TimeLine from "../components/aboutContainer/timeLine";
+import Layout, { siteTitle } from "../components/layout";
+import ResponsiveComponent from "../components/responsiveComponent/responsive";
+import styles from "../styles/page_styles/about.module.scss";
 
+const title: string = siteTitle + " - About Me";
 
-
-const title:string = siteTitle+" - About Me";
-
-
-export default function About({
- 
-}) {
+export default function About({}) {
+  let age: number = new Date().getFullYear() - 1998;
   return (
-
     <Layout home>
-
       <Head>
-  <title>{title}</title>
-
+        <title>{title}</title>
       </Head>
       <div className={styles.mainContainer}>
-
         {AboutContainer({
           img_url: "/images/IMG_6812.jpg",
           position1: 0,
           position2: 70,
           children: [
-            <div style={{paddingTop:"25vh"}}>
-            
-            {ResponsiveComponent([
-              <div className={styles.profileContainer}>
-              <a target="_blank" className={styles.profilebox}>
-              
-              <img src="/images/profile.jpg" className={styles.profile_picture}></img>
-                        </a>
-                        <div className={styles.intro}>
-                Yu Hang Ang, 22, an final year computer science student from Monash University Malaysia. 
-                I'm currently working on my own projects in web app and mobiles app and looking forward to opportunities in software development and data science fields.
-                  
-                </div>
+            <div style={{ paddingTop: "25vh" }}>
+              {ResponsiveComponent([
+                <div className={styles.profileContainer}>
+                  <a target="_blank" className={styles.profilebox}>
+                    <img
+                      src="/images/profile.jpg"
+                      className={styles.profile_picture}
+                    ></img>
+                  </a>
+                  <div className={styles.intro}>
+                    Yu Hang Ang ,{age}, an software engineer based in Kuala
+                    Lumpur, Malaysia.
                   </div>
-              
-            ]
-              )
-
-          }
-</div>
-
-          ]
+                </div>,
+              ])}
+            </div>,
+          ],
         })}
         {ResponsiveComponent([
           <TimeLine
             title="Experiences"
             items={[
-              { dateTime: "7/2021 -", title: "Flutter Developer, Artisan IT Solutions", detail:
-              ""
-            },
-              { dateTime: "11/2020 - 2/2021", title: "Mobile Application Intern, Fehux", detail:
-              "Developing and testing user interfaces and module using Flutter and React Native. \
-              Refactoring and improving existing features and module."
-            },
-             
-              { dateTime: "11/2019 - 2/2020", title: "Javascript Developer Intern, SmartB Solutions", detail:
-                "Collaborated with fellows to developing and maintaining customized features for clients."
+              {
+                dateTime: "Feb 2022 - now",
+                title: "Software Engineer, Snappymob Sdn Bhd",
+                detail: (
+                  <>
+                    Working as vendor developer in My Astro application,
+                    digitalise user managements of Astro products, collaborating
+                    with different stakeholders, Participated in development and
+                    maintenance of new reward modules; integrated CMS, remote
+                    configuration internal and third party APIs.
+                    <br />
+                    <br /> I also worked on user notifications, data caching,
+                    deep linking and debugging and refactoring of existing
+                    product flows.
+                  </>
+                ),
               },
-              { dateTime: "1/12/2018 - 29/2/2020", title: 'Part Time jobs, QMO Academy',
-              detail:"Had a part time jobs as a photographer, videographer and graphic designer"},
-              { dateTime: "1/3/2018", title: '"Hello World!"',detail:"Started to study for computer science degree"}
-              ]}
-          />
-        ,
-        <Skills/>
-      ])}
+              {
+                dateTime: "Jan 2021 - Feb 2022",
+                title: "Mobile Developer, Artisan IT Solutions",
+                detail: (
+                  <>
+                    Worked on an internal applications for idsMED, an
+                    international medical equipment provider, for their
+                    engineers outstations works. The applications stored all
+                    data in relation database (Sqlite), to make sure engineer
+                    able to access inventory, clients, team data and updates the
+                    data during offline. <br /> <br /> Developed and maintained
+                    fews other mobile applications for clients.
+                  </>
+                ),
+              },
+              {
+                dateTime: "Nov 2020 - Feb 2021",
+                title: "Mobile Application Intern, Fehux",
+                detail:
+                  "Developing and testing user interfaces and module using Flutter and React Native. \
+              Refactoring and improving existing features and module.",
+              },
+
+              {
+                dateTime: "Nov 2019 - Feb 2020",
+                title: "Javascript Developer Intern, SmartB Solutions",
+                detail:
+                  "Collaborated with fellows to developing and maintaining customized features for clients.",
+              },
+              {
+                dateTime: "Dec 2018 - Feb 2020",
+                title: "Part Time jobs, QMO Academy",
+                detail:
+                  "Had a part time jobs as a photographer, videographer and graphic designer",
+              },
+              {
+                dateTime: "Jan 2018",
+                title: '"Hello World!"',
+                detail: "Started to study for computer science degree",
+              },
+            ]}
+          />,
+          <Skills />,
+        ])}
       </div>
     </Layout>
-  )
+  );
 }
-
